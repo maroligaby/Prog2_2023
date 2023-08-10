3bbe9f3be18dc30fd3e6757aeb79576e2559dcf9
Gabrielly Maroli <403302@unoesc.lan>
2023-08-10 19:26:15 -0300

    aula 1 e 2

---
+++ PSIU/PSIUWeb/Models/AppUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace PSIUWeb.Models
+{
+    public enum Gender { Feminino, Masculino, Outros }
+    public class AppUser: IdentityUser
+    {
+        [Required(ErrorMessage = "Nome requerido.")]
+        [Display(Name = "Nome completo")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Datade nascimento requerida.")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Gênero/sexo requerido.")]
+        [Display(Name = "Gênero/sexo")]
+        public Gender Gender { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
